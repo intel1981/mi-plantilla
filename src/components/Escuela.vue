61a9d0c4174ea5c6cf2e6a81132cd5b4310d4af9
Weyler Antonio Uicab Pat <intel1981@gmail.com>
2019-01-31 17:17:26 -0500

    Actualicacion Escuela.vue
    Se corrigio la tabla con la vista deseada. Se agregaron datos de prueba. Eso son los datos que se deberian mostrar al usuario.

--- src/components/Escuela.vue
+++ src/components/Escuela.vue
@@ -8,10 +8,15 @@
         </v-toolbar>
 					<v-data-table
 					:headers="headers"
-					:items="desserts"
+					:items="escuelas"
 					class="elevation-1">
-						<template slot="items" slot-scope="props">
-							<td>{{ props.item.name }}</td>
+						<template slot="items" slot-scope="props">              
+							<td class="text-xs-right">{{ props.item.posicion }}</td>
+              <td>{{ props.item.escuela }}</td>
+              <td class="text-xs-center">{{ props.item.clave }}</td>              
+              <td class="text-xs-center">{{ props.item.nivel }}</td>
+              <td class="text-xs-center">{{ props.item.turno }}</td>             
+              <td class="text-xs-center">{{ props.item.status }}</td>
 						</template>
 					</v-data-table>
 			</v-flex>
@@ -25,19 +30,45 @@ export default {
   data () {
       return {
         headers: [
-          {
-            text: 'Nombre',
-            align: 'left',
-            sortable: false,
-            value: 'name'
-          }
+          {text: '#', value: 'posicion', sortable: false, width: '5%'},
+          {text: 'Escuela', value: 'escuela', align: 'left', sortable: false, width: '60%'},          
+          {text: 'CCT', value: 'clave', align: 'center', sortable: false, width: '5%'},          
+          {text: 'Nivel', value: 'nivel', align: 'center', sortable: false, width: '20%'},
+          {text: 'Turno', value: 'turno',  align: 'center', sortable: false, width: '5%'},         
+          {text: 'Estado', value: 'status', align: 'center', sortable: false, width: '5%'}
         ],
-        desserts: [
+        escuelas: [
           {
-            name: 'Centro De Atención Para Estudiantes Con Discapacidad'
+            posicion: '1',
+            clave:'23DCM0033Z',
+            escuela:'Centro de Estudios Tecnológicos del Mar Num. 33',
+            nivel:'Media Superior',
+            turno:'Matutino',            
+            status: true
           },
           {
-            name: 'Centro de Bachillerato Técnologico Industrial Y De Servicios Num. 28'
+            posicion: '2',
+            clave:'23DCM0033Z',
+            escuela:'Centro de Estudios Tecnológicos del Mar Num. 33',
+            nivel:'Media Superior',
+            turno:'Vespertino',            
+            status: true
+          },
+          {
+            posicion: '3',
+            clave:'23DCT0159Q',
+            escuela:'Centro de Bachillerato Tecnológico Industrial y de Servicios Num. 28',
+            nivel:'Media Superior',
+            turno:'Matutino',            
+            status: true
+          },
+          {
+            posicion: '4',
+            clave:'23DCT0159Q',
+            escuela:'Centro de Bachillerato Tecnológico Industrial y de Servicios Num. 28',
+            nivel:'Media Superior',
+            turno:'Vespertino',           
+            status: true
           }
         ]
       }
