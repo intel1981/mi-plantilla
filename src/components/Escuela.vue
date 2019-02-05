e733791d720f754cb20693b1655133d5c399d00b
Weyler Antonio Uicab Pat <intel1981@gmail.com>
2019-02-05 18:05:27 -0500

    Actualizacion Escuela.Vue
    Actualizacion Escuela.Vue

--- src/components/Escuela.vue
+++ src/components/Escuela.vue
@@ -5,6 +5,26 @@
 				<v-toolbar dark color="blue" dense>
           <v-icon large color="white">ballot</v-icon>
           <v-toolbar-title>Escuelas</v-toolbar-title>
+          <v-divider class="mx-2" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <v-btn slot="activator" fab dark small color="green">
+              <v-icon dark>add</v-icon>
+            </v-btn>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container grid-list-md>
+                  <v-layout row wrap>
+                    Controles del formulario
+                  </v-layout>                  
+                </v-container>
+              </v-card-text>
+            </v-card>            
+          </v-dialog>
+          
         </v-toolbar>
 					<v-data-table
 					:headers="headers"
@@ -16,7 +36,16 @@
               <td class="text-xs-center">{{ props.item.clave }}</td>              
               <td class="text-xs-center">{{ props.item.nivel }}</td>
               <td class="text-xs-center">{{ props.item.turno }}</td>             
-              <td class="text-xs-center">{{ props.item.status }}</td>
+              <td class="text-xs-center">
+                <v-icon small color="green" v-if="props.item.status">done</v-icon>
+                <v-icon v-else small color="red">clear</v-icon>
+              </td>
+              <td class="justify-center layout px-0">
+                <v-icon small class="mr-2" color="indigo darken-1" @click="editItem(props.item)">
+                  edit
+                </v-icon>
+                <v-icon small color="red darken-1">delete</v-icon>
+              </td>
 						</template>
 					</v-data-table>
 			</v-flex>
@@ -27,17 +56,53 @@
 <script>
 export default {
   name: "Escuela",
-  data () {
-      return {
-        headers: [
+  data: () => ({
+    dialog: false,
+    headers: [
           {text: '#', value: 'posicion', sortable: false, width: '5%'},
           {text: 'Escuela', value: 'escuela', align: 'left', sortable: false, width: '60%'},          
           {text: 'CCT', value: 'clave', align: 'center', sortable: false, width: '5%'},          
-          {text: 'Nivel', value: 'nivel', align: 'center', sortable: false, width: '20%'},
+          {text: 'Nivel', value: 'nivel', align: 'center', sortable: false, width: '15%'},
           {text: 'Turno', value: 'turno',  align: 'center', sortable: false, width: '5%'},         
-          {text: 'Estado', value: 'status', align: 'center', sortable: false, width: '5%'}
-        ],
-        escuelas: [
+          {text: 'Estado', value: 'status', align: 'center', sortable: false, width: '5%'},
+          {text: 'Acciones', sortable: false, width: '5%'},
+
+    ],
+    escuelas: [],
+    editedIndex: -1,
+    editedItem:{
+      posicion:'',
+      escuela:'',
+      clave:'',
+      nivel:'',
+      turno:'',
+      status:''
+    },
+    defaultItem:{
+      posicion:'',
+      escuela:'',
+      clave:'',
+      nivel:'',
+      turno:'',
+      status:''
+    }
+  }),
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'Nueva Escuela' : 'Editar Escuela'
+    }
+  },
+  watch: {
+    dialog (val) {
+      val || this.close()
+    }
+  },
+  created () {
+    this.initialize()
+  },
+  methods: {
+    initialize () {
+      this.escuelas = [
           {
             posicion: '1',
             clave:'23DCM0033Z',
@@ -52,7 +117,7 @@ export default {
             escuela:'Centro de Estudios Tecnológicos del Mar Num. 33',
             nivel:'Media Superior',
             turno:'Vespertino',            
-            status: true
+            status: false
           },
           {
             posicion: '3',
@@ -70,9 +135,21 @@ export default {
             turno:'Vespertino',           
             status: true
           }
-        ]
-      }
+      ]
+    },
+    editItem (item) {
+      this.editedIndex = this.escuelas.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+    close () {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      }, 300)
     }
+  }
 }
 </script>
 
